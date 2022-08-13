7fabf0c68cd14d796d371af97d0e6a218dfd03f1
pedro <pedro.moreno@aluno.ufabc.edu.br>
2022-08-13 14:37:41 -0300

    Primeira versao

--- codificador.vhd
+++ codificador.vhd
@@ -1,4 +1,3 @@
--- teste git push
 library ieee;
 use ieee.std_logic_1164.all;
 
@@ -7,30 +6,32 @@ entity codificador
 		clk : in std_logic;
 		input : in std_logic;
 		output : out std_logic;
-	valid_out : out std_logic
+		valid_out : out std_logic
 	);
 end entity codificador;
 
 architecture Comportamental of codificador is
-	signal a: std_logic_vector(0 to 60);
-	signal s: std_logic_vector(0 to 5);
+	signal a: std_logic_vector(0 downto 59) := (others => '0'); --inicializa o vetor com '0's
+	signal s: std_logic_vector(0 downto 5);
 	signal P: std_logic;
 begin
-	calcula_paridade: process(clk)
-			variable idx : integer := 0;
+	cod_process: process(clk)
+		variable s_idx : integer := 0;
+		variable a_idx : integer := 0;
 	begin
 		if clk'event and clk = '1' then
-			s(idx) <= input;
-			idx := idx + 1;
-			if idx = 6 then
-				output <= NOT(s(0) xor s(1) xor s(2) xor s(3) xor s(4) xor s(5));
-				idx := 0;
+			s(s_idx) <= input;
+			a(a_idx) <= input;
+			s_idx := s_idx + 1;
+			if s_idx = 6 then
+				a_idx := a_idx + 1;
+				a(a_idx) <= not(s(0) xor s(1) xor s(2) xor s(3) xor s(4) xor s(5));
+				s_idx := 0;
 			end if;
+			-- this might be a problem
+			output <= a(0);
+			a <= a(0 downto 58) & '0'; -- left shift
 		end if;
-	end process;
-	
-	buffer_saida: process(clk)
-	begin
 	end process;
 end Comportamental;
 
