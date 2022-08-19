29e6b0a057614264a5f3c77d69dbb0a1ecfd61df
pedro <pedro.moreno@aluno.ufabc.edu.br>
2022-08-19 05:41:48 -0300

    v1.0

--- codificador.vhd
+++ codificador.vhd
@@ -11,57 +11,92 @@ entity codificador is
 	);
 end entity codificador;
 
-architecture Comportamental of codificador is
-	signal a: std_logic_vector(0 to 59); --inicializa o vetor com '0's
-	signal s: std_logic_vector(0 to 5) := "000000";
-	signal P: std_logic;
-	signal s_idx: integer := -1;
-	signal a_idx: integer := 0;
+architecture rtl of codificador is
+	component contador_3b is
+		port (
+			clk: in std_logic;
+			output: out std_logic_vector(0 to 2)
+		) ;
+	end component;
 
-	signal teste: std_logic_vector(0 to 1) := "00";
+	component decoder_8b is
+		port (
+			input : in std_logic_vector(0 to 2);
+			output : out std_logic_vector(0 to 7)
+		) ;
+	end component;
+
+	component mod_reg_8b_serial is
+		port (
+			main_in, sec_in, clk : in std_logic;
+			state : in std_logic_vector(0 to 7);
+			output : out std_logic
+		) ;
+	end component;
+
+	component paridade_impar_6b is
+		port (
+			input : in std_logic_vector(0 to 5);
+			output: out std_logic
+		) ;
+	end component;
+
+	component period_changer_6 is
+		port (
+			clk: in std_logic;
+			output: out std_logic
+		);
+	end component;
+
+	component serial_reg_6b_parallel is
+		port(
+			input, clk : in std_logic;
+			output : out std_logic_vector(0 to 5)
+		);
+	end component;
+
+	signal contador_3b_out : std_logic_vector(0 to 2);
+	signal decoder_8b_out : std_logic_vector(0 to 7);
+	signal par_out : std_logic;
+	signal pchanger_out : std_logic;
+	signal srg6_out : std_logic_vector(0 to 5);
+
 begin
-	
-	P <= not(s(0) xor s(1) xor s(2) xor s(3) xor s(4) xor s(5));
-	a(a_idx mod 60) <= input;
-	a((a_idx + 1) mod 60) <= P;
+	srg6: serial_reg_6b_parallel port map(
+		input => input,
+		clk => clk,
+		output => srg6_out
+	);
 
-	teste(a_idx mod 2) <= input;
-	teste((a_idx + 1) mod 2) <= P;
+	par: paridade_impar_6b port map(
+		input => srg6_out,
+		output => par_out
+	);
 
-	s(s_idx mod 6) <= input;
-	output <= a(0);
-	with input select
-		valid_out <=
-			'1' when '0' | '1',
-			'0' when others;
-	-- valid_out <= '1' when (input = '1' or input = '0') else	'0';
-	
-	teste_process: process(clk)
-	begin
-		if clk'event and clk = '1' then
-			s_idx <= (s_idx + 1);
-			if s_idx = 5 then
-				a_idx <= (a_idx + 1);
-				-- a(teste_a mod 60) <= P;
-				s_idx <= 0;
-			end if ;
-		end if ;
-	end process;
+	clk6: period_changer_6 port map(
+		clk => clk,
+		output => pchanger_out
+	);
 
-	-- cod_process: process(clk)
-	-- 	variable aux : integer := 0;
-	-- 	variable debug_count : integer := 0;
-	-- begin
-	-- 	if clk'event and clk = '1' then
-	-- 		s_idx := (s_idx + 1) mod 6;
-	-- 		aux := aux + 1;
-	-- 		if aux = 6 then
-	-- 			a_idx := a_idx + 1;
-	-- 			aux := 0;
-	-- 		end if;
-	-- 		-- a <= a(1 to 59) & '0'; -- left shift
-	-- 	end if;
-	-- end process;
-	
-end Comportamental;
+	contador: contador_3b port map(
+		clk => pchanger_out,
+		output => contador_3b_out
+	);
+
+	decoder: decoder_8b port map(
+		input => contador_3b_out,
+		output => decoder_8b_out
+	);
+
+	modsrg8: mod_reg_8b_serial port map(
+		main_in => input,
+		sec_in => par_out,
+		clk => clk,
+		state => decoder_8b_out,
+		output => output
+	);
+
+	valid_out <= '1';
+
+end rtl;
 
