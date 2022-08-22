334e575f63f1f2dffb1d97a02e90f2c38bbaa7c9
pedro <pedro.moreno@aluno.ufabc.edu.br>
2022-08-22 10:03:02 -0300

    added '0' initialization

--- flipflopD.vhd
+++ flipflopD.vhd
@@ -9,10 +9,15 @@ entity flipflopD is
 end flipflopD;
 
 architecture bhv of flipflopD is
+  signal reset : std_logic := '0';
 begin
     ff : process
     begin
+      if reset = '0' then
+        Q <= '0';
+      end if ;
       wait until clk'event and clk = '1';
       Q <= D;
+      reset <= '1';
     end process ; -- ff
 end bhv ; -- bhv
